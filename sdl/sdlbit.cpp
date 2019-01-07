577cf51b9e64f01cffcb5ae75e9e16b538b69c74
cardiffman <cardiffman@yahoo.com>
2019-01-06 19:59:02 -0800

    Add shared anim to SDL

--- sdl/sdlbit.cpp
+++ sdl/sdlbit.cpp
@@ -14,52 +14,15 @@
 #include <SDL_ttf.h>
 #include "res_path.h"
 #include "cleanup.h"
+#include "scene.h"
+#include "scene_builder.h"
 
 using std::vector;
+using std::cout;
+using std::endl;
 
 const int SCREEN_WIDTH  = 1280;
-const int SCREEN_HEIGHT = 720;
-
-//We'll be scaling our tiles to be 40x40
-const int TILE_SIZE = 40;
-
-namespace SDLBIT {
-struct Asset {
-	unsigned id; SDL_Texture* image; unsigned width; unsigned height; unsigned rowbytes;
-};
-struct Glyph {
-	unsigned id; unsigned parent_id; unsigned asset_id; int x; int y; unsigned width; unsigned height; uint32_t color; unsigned children;
-};
-struct Scene {
-	vector<Asset> assets;
-	vector<Glyph> glyphs;
-};
-}
-static const unsigned NO_PAR = 0;
-SDLBIT::Scene scene = {
-		{
-		{0},
-		{ 1, NULL, 1280, 720, 1280*4 },
-		{ 2, NULL, 100, 100, 100*4 }
-		},
-		{
-				{0},
-				{ 1, NO_PAR, 0, 0, 0, 1280, 720 ,0xff0000FF},
-				{ 2, NO_PAR, 0, 0, 0, 100, 100,0xffFFFF00},
-				{ 3, NO_PAR, 0, 100,100,1080,520 ,0xff808080},
-				{ 4, 3,      0, 0, 0, 100, 100,0xffFF00FF},
-				{ 5, 3,      0, 100, 0, 100, 100,0xff808080},
-				{ 6, 3,      0, 200, 0, 100, 100,0xff808080},
-				{ 7, 3,      0, 300, 0, 100, 100,0xff808080},
-				{ 8, 3,      0, 400, 0, 100, 100,0xff808080},
-				{ 9, 3,      0, 500, 0, 100, 100,0xff808080},
-				{ 10, 3,     0, 600, 0, 100, 100,0xff808080},
-				{ 11, 3,     0, 700, 0, 100, 100,0xff808080},
-				{ 12, 3,     0, 800, 0, 100, 100,0xff808080},
-				{ 13, 3,     0, 900, 0, 100, 100,0xff808080},
-				{ 14, 3,     0, 1000, 0, 100, 100,0xff808080},
-		}
-};
+const int SCREEN_HEIGHT = SCREEN_HEIGHT;
 
 /**
 * Log an SDL error with some error message to the output stream of our choice
@@ -70,92 +33,6 @@ void logSDLError(std::ostream &os, const std::string &msg){
 	os << msg << " error: " << SDL_GetError() << std::endl;
 }
 
-/*
- * Loads an image into a texture on the rendering device
- * @param file The image file to load
- * @param ren The renderer to load the texture onto
- * @return the loaded texture, or nullptr if something went wrong.
- */
-SDL_Texture* loadTexture(const std::string &file, SDL_Renderer *ren){
-	SDL_Texture *texture = IMG_LoadTexture(ren, file.c_str());
-	if (texture == nullptr){
-		logSDLError(std::cout, "LoadTexture");
-	}
-	return texture;
-}
-/**
-* Draw an SDL_Texture to an SDL_Renderer at position x, y, with some desired
-* width and height
-* @param tex The source texture we want to draw
-* @param ren The renderer we want to draw to
-* @param x The x coordinate to draw to
-* @param y The y coordinate to draw to
-* @param w The width of the texture to draw
-* @param h The height of the texture to draw
-*/
-void renderTexture(SDL_Texture *tex, SDL_Renderer *ren, int x, int y, int w, int h){
-	//Setup the destination rectangle to be at the position we want
-	SDL_Rect dst;
-	dst.x = x;
-	dst.y = y;
-	dst.w = w;
-	dst.h = h;
-	SDL_RenderCopy(ren, tex, NULL, &dst);
-}
-/**
-* Draw an SDL_Texture to an SDL_Renderer at position x, y, preserving
-* the texture's width and height
-* @param tex The source texture we want to draw
-* @param ren The renderer we want to draw to
-* @param x The x coordinate to draw to
-* @param y The y coordinate to draw to
-*/
-void renderTexture(SDL_Texture *tex, SDL_Renderer *ren, int x, int y){
-	//Setup the destination rectangle to be at the position we want
-	SDL_Rect dst;
-	dst.x = x;
-	dst.y = y;
-	//Query the texture to get its width and height to use
-	SDL_QueryTexture(tex, NULL, NULL, &dst.w, &dst.h);
-	SDL_RenderCopy(ren, tex, NULL, &dst);
-}
-
-/*
- * Draw an SDL_Texture to an SDL_Renderer at some destination rect
- * taking a clip of the texture if desired
- * @param tex The source texture we want to draw
- * @param rend The renderer we want to draw too
- * @param dst The destination rectangle to render the texture too
- * @param clip The sub-section of the texture to draw (clipping rect)
- *		default of nullptr draws the entire texture
- */
-void renderTexture(SDL_Texture *tex, SDL_Renderer *ren, SDL_Rect dst, SDL_Rect *clip = nullptr){
-	SDL_RenderCopy(ren, tex, clip, &dst);
-}
-/*
- * Draw an SDL_Texture to an SDL_Renderer at position x, y, preserving
- * the texture's width and height and taking a clip of the texture if desired
- * If a clip is passed, the clip's width and height will be used instead of the texture's
- * @param tex The source texture we want to draw
- * @param rend The renderer we want to draw too
- * @param x The x coordinate to draw too
- * @param y The y coordinate to draw too
- * @param clip The sub-section of the texture to draw (clipping rect)
- *		default of nullptr draws the entire texture
- */
-void renderTexture(SDL_Texture *tex, SDL_Renderer *ren, int x, int y, SDL_Rect *clip){
-	SDL_Rect dst;
-	dst.x = x;
-	dst.y = y;
-	if (clip != nullptr){
-		dst.w = clip->w;
-		dst.h = clip->h;
-	}
-	else {
-		SDL_QueryTexture(tex, NULL, NULL, &dst.w, &dst.h);
-	}
-	renderTexture(tex, ren, dst, clip);
-}
 /*
  * Render the message we want to display to a texture for drawing
  * @param message The message we want to display
@@ -192,44 +69,10 @@ SDL_Texture* renderText(const std::string &message, const std::string &fontFile,
 	return texture;
 }
 
-namespace SDLBIT {
+Scene scene;
+BitBuffer screen;
 
-void draw_scene(SDL_Renderer* renderer)
-{
-	for (auto g : scene.glyphs) {
-		g.children = 0;
-	}
-	for (auto g : scene.glyphs) {
-		if (g.parent_id != NO_PAR)
-			scene.glyphs[g.parent_id].children++;
-	}
-
-	for (auto g : scene.glyphs) {
-		int offsetx = 0;
-		int offsety = 0;
-		if (g.children)
-			continue;
-		if (g.parent_id != NO_PAR)
-		{
-			offsetx = scene.glyphs[g.parent_id].x;
-			offsety = scene.glyphs[g.parent_id].y;
-		}
-		if (g.asset_id)
-		{
-			renderTexture(scene.assets[g.asset_id].image, renderer, g.x+offsetx, g.y+offsety);
-		}
-		else
-		{
-			SDL_Rect r; r.x = g.x+offsetx; r.y = g.y+offsety; r.w = g.width; r.h = g.height;
-			SDL_SetRenderDrawColor(renderer, (g.color&0x000FF0000)>>16,(g.color&0x00000FF00)>>8, (g.color&0x0000000FF),
-					(g.color&0x0FF000000)>>24);
-			SDL_RenderFillRect(renderer, &r);
-		}
-	}
-}
-}
-
-int main(int, char**){
+int main(int argc, char** argv){
 	//Start up SDL and make sure it went ok
 	if (SDL_Init(SDL_INIT_VIDEO) != 0){
 		logSDLError(std::cout, "SDL_Init");
@@ -237,7 +80,7 @@ int main(int, char**){
 	}
 
 	//Setup our window and renderer
-	SDL_Window *window = SDL_CreateWindow("Lesson 5", SDL_WINDOWPOS_CENTERED,
+	SDL_Window *window = SDL_CreateWindow("sdlbit", SDL_WINDOWPOS_CENTERED,
 			SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
 	if (window == nullptr){
 		logSDLError(std::cout, "CreateWindow");
@@ -251,78 +94,92 @@ int main(int, char**){
 		SDL_Quit();
 		return 1;
 	}
-	const std::string resPath = getResourcePath("Lesson5");
-	SDL_Texture *image = loadTexture(resPath + "image.png", renderer);
-	if (image == nullptr){
-		cleanup(image, renderer, window);
-		IMG_Quit();
-		SDL_Quit();
-		return 1;
-	}
+#if 1 // Enable interesting code when event loop is debugged
+    int width=SCREEN_WIDTH, height=SCREEN_HEIGHT;
 
-	//iW and iH are the clip width and height
-	//We'll be drawing only clips so get a center position for the w/h of a clip
-	int iW = 100, iH = 100;
-	int x = SCREEN_WIDTH / 2 - iW / 2;
-	int y = SCREEN_HEIGHT / 2 - iH / 2;
+    screen.rowbytes = width*4;
+    screen.dims.width = width;
+    screen.dims.height = height;
+    screen.mem = (uint8_t*)malloc(screen.rowbytes * screen.dims.height);
+    memset(screen.mem, 0, screen.rowbytes * screen.dims.height);
 
-	//Setup the clips for our image
-	SDL_Rect clips[4];
-	//Since our clips our uniform in size we can generate a list of their
-	//positions using some math (the specifics of this are covered in the lesson)
-	for (int i = 0; i < 4; ++i){
-		clips[i].x = i / 2 * iW;
-		clips[i].y = i % 2 * iH;
-		clips[i].w = iW;
-		clips[i].h = iH;
+	SceneBuilder builder;
+	if (argc > 1)
+	{
+		try {
+			builder.parse_containers(argv[1]);
+		} catch (char const * ex) {
+			cout << "Exception " << ex << endl;
+			return 1;
+		} catch (const std::string& ex) {
+			cout << "Exception " << ex << endl;
+			return 1;
+		}
+		scene.containers = builder.nc;
+		scene.assets = builder.na;
 	}
-	//Specify a default clip to start with
-	int useClip = 0;
+	else
+	{
+		scene.containers.push_back(Container());
+		scene.containers.push_back(Container({{ 0,0,SCREEN_WIDTH,SCREEN_HEIGHT },1,0,0,0xFFFF0000}));
+		scene.assets = {{0}};
+	}
+#if 1
+    auto sdlTexture = SDL_CreateTexture(renderer,
+                                   SDL_PIXELFORMAT_ARGB8888,
+                                   SDL_TEXTUREACCESS_STREAMING,
+                                   SCREEN_WIDTH, SCREEN_HEIGHT);
+
+    SDL_LockTexture(sdlTexture, NULL, reinterpret_cast<void**>(&screen.mem), reinterpret_cast<int*>(&screen.rowbytes));
+    draw_scene(scene,screen);
+    SDL_UnlockTexture(sdlTexture);
+    SDL_RenderCopy(renderer, sdlTexture, NULL, NULL);
+#else
+    draw_scene(scene,screen);
+    auto sdlTexture = SDL_CreateTexture(renderer,
+                                   SDL_PIXELFORMAT_ARGB8888,
+                                   SDL_TEXTUREACCESS_STREAMING,
+                                   SCREEN_WIDTH, SCREEN_HEIGHT);
+    SDL_Rect sdlRect; sdlRect.x = 0; sdlRect.y = 0; sdlRect.w = SCREEN_WIDTH; sdlRect.h = SCREEN_HEIGHT;
+    cout << "Texture created " << sdlTexture <<endl;
+    int res = SDL_UpdateTexture(sdlTexture, NULL, screen.mem, screen.dims.height*screen.rowbytes);
+    cout << "Texture update " << res <<endl;
+    SDL_RenderCopy(renderer, sdlTexture, NULL, NULL);
+#endif
+#endif
+	SDL_RenderPresent(renderer);
 
 	SDL_Event e;
 	bool quit = false;
 	while (!quit){
 		//Event Polling
-		while (SDL_PollEvent(&e)){
+		if (SDL_WaitEvent(&e)){
 			if (e.type == SDL_QUIT){
 				quit = true;
-			}
-			//Use number input to select which clip should be drawn
-			if (e.type == SDL_KEYDOWN){
-				switch (e.key.keysym.sym){
-					case SDLK_1:
-					case SDLK_KP_1:
-						useClip = 0;
-						break;
-					case SDLK_2:
-					case SDLK_KP_2:
-						useClip = 1;
-						break;
-					case SDLK_3:
-					case SDLK_KP_3:
-						useClip = 2;
-						break;
-					case SDLK_4:
-					case SDLK_KP_4:
-						useClip = 3;
-						break;
-					case SDLK_ESCAPE:
-						quit = true;
-						break;
-					default:
-						break;
-				}
+			} else if (e.type == SDL_KEYDOWN) {
+				;
+			} else if (e.type == SDL_KEYUP) {
+				;
 			}
 		}
 		//Rendering
+#if 1
+	    SDL_LockTexture(sdlTexture, NULL, reinterpret_cast<void**>(&screen.mem), reinterpret_cast<int*>(&screen.rowbytes));
+	    draw_scene(scene,screen);
+	    SDL_UnlockTexture(sdlTexture);
+	    SDL_RenderCopy(renderer, sdlTexture, NULL, NULL);
+		SDL_RenderPresent(renderer);
+#else
 		SDL_RenderClear(renderer);
 		//Draw the image
-		SDLBIT::draw_scene(renderer);
+		draw_scene(scene, screen);
+	    SDL_UpdateTexture(sdlTexture, &sdlRect, screen.mem, screen.rowbytes);
 		//Update the screen
 		SDL_RenderPresent(renderer);
+#endif
 	}
 	//Clean up
-	cleanup(image, renderer, window);
+	cleanup(renderer, window);
 	IMG_Quit();
 	SDL_Quit();
 	return 0;
