13ae2d398c02de5c43a5b3e2ccb64805640d9d88
cardiffman <cardiffman@yahoo.com>
2019-02-01 14:48:17 -0800

    Files for opengl tutorial

---
+++ opengl/sprites.v.glsl
@@ -0,0 +1,10 @@
+#version 440 compatibility
+attribute vec4 v_coord;
+attribute vec2 v_texcoord;
+varying vec2 f_texcoord;
+uniform mat4 mvp;
+
+void main(void) {
+  gl_Position = mvp * v_coord;
+  f_texcoord = v_texcoord;
+}
