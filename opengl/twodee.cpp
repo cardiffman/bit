833948fa72a2110375bfbac1f64a8ae0c38ad74e
cardiffman <cardiffman@yahoo.com>
2019-03-04 08:11:24 -0800

    Make Opengl better

--- opengl/twodee.cpp
+++ opengl/twodee.cpp
@@ -2,9 +2,14 @@
 #include <GL/freeglut.h>
 #include <iostream>
 #include <stdint.h>
+#include "engine.h"
+#include "scene_builder.h"
 
 using std::cout;
 using std::endl;
+
+#define NLOG(A) cout << __FUNCTION__ << ' ' << A << ' ' << __FILE__ << ":" << __LINE__ << endl
+
 #define SCREEN_WIDTH 1280
 #define SCREEN_HEIGHT 720
 
@@ -18,7 +23,7 @@ void render()
     glLoadIdentity();
     //glTranslatef( x, y, 0);
     glBindTexture(GL_TEXTURE_2D, texID);
-    cout << "Binding " << texID << endl;
+    NLOG("Binding " << texID);
         glBegin( GL_QUADS );
             glTexCoord2f( 0.f, 0.f ); glVertex2f(           0.f,            0.f );
             glTexCoord2f( 1.f, 0.f ); glVertex2f( SCREEN_WIDTH,            0.f );
@@ -46,21 +51,50 @@ int main(int argc, char** argv)
     GLenum error = glGetError();
     if (error != GL_NO_ERROR)
     {
-        cout << gluErrorString(error) << endl;
+        NLOG(gluErrorString(error));
         return 1;
     }
+	GraphicsEngine* engine = init_base_engine();
+	Scene scene;
+	SceneBuilder builder;
+    if (argc > 1)
+    {
+        try {
+            builder.parse_containers(argv[1], engine);
+        } catch (char const * ex) {
+            cout << "Exception " << ex << endl;
+            return 1;
+        } catch (const std::string& ex) {
+            cout << "Exception " << ex << endl;
+            return 1;
+        }
+        scene.containers = builder.nc;
+        scene.assets = builder.na;
+    }
+    else
+    {
+        scene.containers.push_back(Container());
+        scene.containers.push_back(Container({{ 0,0,1280,720 },1,0,0,0xFF00FF00}));
+        scene.assets.push_back(Asset());
+    }
+    draw_scene(scene, engine);
+	GraphicsBuffer* screenBuffer = engine->getScreenBuffer();
+	uint8_t* screen; uint32_t pitch;
+	screenBuffer->lock(screen, pitch);
+#if 0
     uint32_t* screen = new uint32_t[SCREEN_WIDTH*SCREEN_HEIGHT];
     for (unsigned y=0; y<SCREEN_HEIGHT; ++y)
         for (unsigned x=0; x<SCREEN_WIDTH; ++x)
         {
             screen[y*SCREEN_WIDTH+x] = ((x ^ y) & 1) ? 0xFF000000 : 0xFFFFFFFF;
         }
+#endif
     glGenTextures(1, &texID);
-    cout << "Generated " << texID << endl;
+    NLOG("Generated " << texID);
     glBindTexture(GL_TEXTURE_2D, texID);
-    cout << "Binding " << texID << endl;
+    NLOG("Binding " << texID);
     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, SCREEN_WIDTH, SCREEN_HEIGHT, 0, GL_RGBA, GL_UNSIGNED_BYTE, screen);
-    cout << "TEXiMAGE" << endl;
+    NLOG("TEXiMAGE");
 
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
@@ -68,7 +102,7 @@ int main(int argc, char** argv)
     error = glGetError();
     if( error != GL_NO_ERROR )
     {
-        cout << "Error loading texture from " <<screen <<" pixels! " << gluErrorString( error ) << endl;
+        NLOG("Error loading texture from " <<screen <<" pixels! " << gluErrorString( error ));
         return false;
     }
 
